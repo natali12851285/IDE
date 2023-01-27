da13c881b6099d6bf9468ecf3ec8361fdc28e5f7
Name <alex12851285@outlook.com>
2023-01-27 21:53:03 +0300

    5 fix

---
+++ guess-number-task/game_Task_8.1.py/game_Task_8.1.1.py
@@ -0,0 +1,33 @@
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+import numpy as np
+count = 0
+number = np.random.randint(1, 101) # загадали список чисел
+print("Компьютер сам загадывает и сам угадывает число от 1 до 100")
+min = 0
+max = 100
+while True:
+    predict = round((min+max)/2)
+    count += 1
+    if number == predict:
+        break
+    elif number > predict:
+        min = predict
+        print(f"Угадываемое число больше {predict}")
+        print ({round((max + min) / 2)})
+    elif number < predict:
+        max = predict
+        print(f"Угадываемое число меньше {predict}")
+        print ({round((max+min)/2)})
+print(f"Вы угадали число {number} за {count} попыток.")
+
+    # Ваш код заканчивается здесь
+
+#return count
